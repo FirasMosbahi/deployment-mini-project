3de1a605933094c5d578164cf8b377bde37a0d33
firas <firasmosbahi10@gmail.com>
2024-11-19 20:46:41 +0100

    change replicas to 1

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const todoRoutes = require("./routes");
 
 const app = express();
-app.use(express.json()); // Middleware to parse JSON bodies
-app.use("/api/todos", todoRoutes); // Use the todo routes
+app.use(express.json());
+app.use("/api/todos", todoRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
